10e2b6f06ed3891d61a925fd1ef9a18f3db7af44
Johns Gaming PC <1998johngtz@gmail.com>
2023-07-02 22:26:40 -0500

    finished password generator with script commments for devs

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# m3-chall-psw-gen
+# Password Generator Project 3
+
+This is a random password generator created with desired user criterias as prompted.
+
+## Table of Contents
+
+- [Website link](#Websitelink)
+- [Features](#features)
+- [Installation](#installation)
+
+## Website Link
+
+
+
+## Features
+
+- List the key features of your application or project here.
+
+## Installation
+
+1. Clone the repository to your local machine.
+2. Open the project in your code editor.
+3. Install the dependencies by running the following command:
+
--- assets/style-js/script.js
+++ assets/style-js/script.js
@@ -1,8 +1,8 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -10,8 +10,56 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Generate password based on user criteria
+function generatePassword() {
+  var length = prompt("Enter the password length (8-128 characters):");
+
+  if (length === null) {
+    return ""; // User canceled the prompt, return empty string
+  }
+
+  length = parseInt(length);
+
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Please enter a valid password length between 8 and 128 characters.");
+    return "";
+  }
+
+  var lowercase = confirm("Include lowercase letters?");
+  var uppercase = confirm("Include uppercase letters?");
+  var numeric = confirm("Include numeric characters?");
+  var special = confirm("Include special characters?");
+
+  if (!lowercase && !uppercase && !numeric && !special) {
+    alert("Please select at least one character type.");
+    return "";
+  }
+
+  var characterSet = "";
+
+  if (lowercase) {
+    characterSet += "abcdefghijklmnopqrstuvwxyz";
+  }
+
+  if (uppercase) {
+    characterSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+
+  if (numeric) {
+    characterSet += "0123456789";
+  }
+
+  if (special) {
+    characterSet += "!@#$%^&*()_+~`|}{[]\\:;?><,./-=";
+  }
+
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * characterSet.length);
+    password += characterSet[randomIndex];
+  }
+
+  return password;
+}
